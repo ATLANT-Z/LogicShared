0bf8ad9dfd7320516ff6d97759c36495da2acebf
Andrey <20skorpion0@gmail.com>
2022-08-19 20:40:56 +0300

    Cart Refactor 50%

--- services/money.view.service.ts
+++ services/money.view.service.ts
@@ -1,7 +1,7 @@
-export class MoneyService {
+export class MoneyViewService {
 	private static numberFormat = Intl.NumberFormat('ru-RU', {maximumFractionDigits: 2, minimumFractionDigits: 2});
 
 	static format(val: number) {
-		return MoneyService.numberFormat.format(val).replace(',', '.');
+		return MoneyViewService.numberFormat.format(val).replace(',', '.');
 	}
 }
