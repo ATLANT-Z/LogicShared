1329f0ed73195ce43e981c92db43066d18d5d5ba
Andrey <20skorpion0@gmail.com>
2022-09-12 16:22:53 +0300

    shared modify

--- tools/globalTools.ts
+++ tools/globalTools.ts
@@ -1,25 +1,8 @@
 import {App} from "vue";
 import {DictionaryWord, LocaleableValue, translateService} from "@shared/services/translate.service";
-import {PopupService, popupService} from "@shared/services/popup.service";
-import {Icon, projectIcons} from "@shared/type/icons";
-import {routeHelper, RouteHelper} from "@shared/helpers/route.helper";
-import {breadcrumbService, BreadcrumbService} from "@shared/services/breadcrumb.service";
+import {projectIcons} from "@shared/type/icons";
 
-type GlobalFunctionList = ReturnType<typeof globalFunctions>
-
-declare module '@vue/runtime-core' {
-	interface ComponentCustomProperties {
-		$globalFunc: GlobalFunctionList;
-		_T: GlobalFunctionList['translate'];
-		getLocaleable: GlobalFunctionList['getLocaleable']
-		icons: Record<Icon, Icon>;
-		routeHelper: RouteHelper
-		popupService: PopupService
-		breadcrumbService: BreadcrumbService
-	}
-}
-
-function globalFunctions(app: App) {
+export function globalFunctions(app: App) {
 	return {
 		translate(word: DictionaryWord, params?: any) {
 			return translateService.getWord(word, {$params: params});
@@ -52,31 +35,9 @@ function globalFunctions(app: App) {
 	}
 }
 
-function createIcons() {
+export function createIcons() {
 	const icons = {};
 	Object.keys(projectIcons).forEach(el => icons[el] = el);
 	return icons;
 }
 
-export default {
-	// eslint-disable-next-line no-unused-vars
-	install(app: App, options: any | unknown) {
-		const func = globalFunctions(app)
-		app.config.globalProperties.$globalFunc = func;
-		app.config.globalProperties._T = func.translate;
-		app.config.globalProperties.getLocaleable = func.getLocaleable;
-
-		app.config.globalProperties.icons = createIcons();
-
-		app.config.globalProperties.routeHelper = routeHelper;
-		app.config.globalProperties.popupService = popupService;
-
-		app.config.globalProperties.breadcrumbService = breadcrumbService;
-
-		// Object.entries(globalFunctions(app)).forEach(el => {
-		// 	const [key, func] = el;
-		// 	console.log(el);
-		// 	app.provide(key, func);
-		// });
-	},
-};
